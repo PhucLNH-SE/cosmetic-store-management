b421d3ceefe144bbbfcf7004c5a2d69c4602f376
Phuc Le <phuclenguyenhoang28@gmail.com>
2026-03-09 17:53:45 +0700

    Initial project structure with gitignore

---
+++ Helpers/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CosmeticStoreManagement.Helpers
+
+{
+    class BaseViewModel
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
